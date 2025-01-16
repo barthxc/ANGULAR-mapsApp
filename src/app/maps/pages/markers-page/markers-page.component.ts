5345a0d1967b316e05fc0d082c4575eb05061740
Pablo <pablo.bartolome@nter.es>
2025-01-16 10:04:16 +0100

    using markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrls: ['./markers-page.component.css']
+  styleUrls: ['./markers-page.component.css'],
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public map?: Map;
+  public currentLngLat?: LngLat = new LngLat(-3.603, 37.16718);
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no se ha encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 17, // starting zoom
+    });
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    new Marker({
+      color,
+    })
+      .setLngLat(this.currentLngLat!)
+      .addTo(this.map);
+  }
 }
